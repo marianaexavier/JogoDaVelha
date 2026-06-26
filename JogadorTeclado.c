31ef062de46ef2d1b84ed7a6d5db92c49b578fba
Helena Couto <helenacdos00@gmail.com>
2026-06-26 20:59:46 -0300

    correção ortografia

--- JogadorTeclado.c
+++ JogadorTeclado.c
@@ -3,21 +3,23 @@
 #include "Tabuleiro.h"
 
 static char distinguirJogador(int tipo) {
-    if (tipo == X) {
-        return 'X';
-    } else {
-        return 'O';
-    }
+    if (tipo == X) return 'X';
+    return 'O';
 }
 
 void joga(int tipo) {
     int linha, coluna;
-    int sucesso;
 
+    do {
     printf("Jogador %c, digite a linha e a coluna (1 a 3): ", distinguirJogador(tipo));
     scanf("%d %d", &linha, &coluna);
     linha--;
     coluna--;
 
+    if (!posicaoDisponivel(linha, coluna)) {
+        printf("Posicao indisponivel. Tente novamente.\n");
+    }
+    } while (!posicaoDisponivel(linha, coluna));
+    
     marcaJogada(linha, coluna, tipo);
 }
