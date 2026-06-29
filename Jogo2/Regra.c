03eaba50129ca91fa473a36825beae3818f79ed6
Helena Couto <helenacdos00@gmail.com>
2026-06-28 21:52:39 -0300

    adiciona arquivos das regras

---
+++ Jogo2/Regra.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "Tabuleiro.h"
+
+int regra1() {
+
+}
+
+int regra2() {
+
+}
+
+int regra3() {
+
+}
+
+int regra4() {
+
+}
+
+int regra5() {
+
+}
+
+int regra6() {
+
+}
---
+++ Jogo2/Regra.h
@@ -0,0 +1,11 @@
+#ifndef REGRA_H
+#define REGRA_H
+
+int regra1();
+int regra2();
+int regra3();
+int regra4();
+int regra5();
+int regra6();
+
+#endif
