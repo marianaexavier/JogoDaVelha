d82cb94a739710311f7d8fcc8bf9a990bd241648
Helena Couto <helenacdos00@gmail.com>
2026-07-02 14:53:54 -0300

    Parâmetro ajustado em jogaIA()

--- Jogo2/JogadorIA.c
+++ Jogo2/JogadorIA.c
@@ -3,10 +3,10 @@
 #include "Tabuleiro.h"
 
 void jogaIA(Tabuleiro *tab, int tipo, int tipoHumano) {
-    if (regra1(&tab, tipo)) return;
-    if (regra2(&tab, tipo)) return;
+    if (regra1(&tab, tipo, tipoHumano)) return;
+    if (regra2(&tab, tipo, tipoHumano)) return;
     if (regra3(&tab, tipo)) return;
-    if (regra4(&tab, tipo)) return;
+    if (regra4(&tab, tipo, tipoHumano)) return;
     if (regra5(&tab, tipo)) return;
     regra6(&tab, tipo);
 }
