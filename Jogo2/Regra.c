4c88616ce7d7c44a2088f24ea534ba9e9780ae4c
Helena Couto <helenacdos00@gmail.com>
2026-07-06 16:59:53 -0300

    replace com tab-> no jogo2

--- Jogo2/Regra.c
+++ Jogo2/Regra.c
@@ -7,7 +7,7 @@ int regra1(Tabuleiro *tab, int tipo, int tipoHumano) {
 
     for (k = 0; k < 2; k++){
         for (i = 0; i < 3; i++) {
-            soma = (*tab).M[i][0] + (*tab).M[i][1] + (*tab).M[i][2];
+            soma = tab->M[i][0] + tab->M[i][1] + tab->M[i][2];
             if (soma == tipo * 2) {
                 for (j = 0; j < 3; j++) {
                     if (posicaoDisponivel(tab, i, j)) {
@@ -19,7 +19,7 @@ int regra1(Tabuleiro *tab, int tipo, int tipoHumano) {
         }
 
         for (j = 0; j < 3; j++) {
-            soma = (*tab).M[0][j] + (*tab).M[1][j] + (*tab).M[2][j];
+            soma = tab->M[0][j] + tab->M[1][j] + tab->M[2][j];
             if (soma == tipo * 2) {
                 for (i = 0; i < 3; i++) {
                     if (posicaoDisponivel(tab, i, j)) {
@@ -30,7 +30,7 @@ int regra1(Tabuleiro *tab, int tipo, int tipoHumano) {
             }
         }
 
-        soma = (*tab).M[0][0] + (*tab).M[1][1] + (*tab).M[2][2];
+        soma = tab->M[0][0] + tab->M[1][1] + tab->M[2][2];
         if (soma == tipo * 2) {
             for (i = 0; i < 3; i++) {
                 if (posicaoDisponivel(tab, i, i)) {
@@ -40,7 +40,7 @@ int regra1(Tabuleiro *tab, int tipo, int tipoHumano) {
             }
         }
 
-        soma = (*tab).M[0][2] + (*tab).M[1][1] + (*tab).M[2][0];
+        soma = tab->M[0][2] + tab->M[1][1] + tab->M[2][0];
         if (soma == tipo * 2) {
             for (i = 0; i < 3; i++) {
                 if (posicaoDisponivel(tab, i, 2 - i)) {
@@ -63,19 +63,19 @@ int regra2(Tabuleiro *tab, int tipo) {
             if (posicaoDisponivel(tab, i, j)) {
                 contSequencias = 0; 
 
-                soma = (*tab).M[i][0] + (*tab).M[i][1] + (*tab).M[i][2];
+                soma = tab->M[i][0] + tab->M[i][1] + tab->M[i][2];
                 if (soma == tipo) contSequencias++;
                     
-                soma = (*tab).M[0][j] + (*tab).M[1][j] + (*tab).M[2][j];
+                soma = tab->M[0][j] + tab->M[1][j] + tab->M[2][j];
                 if (soma ==  tipo) contSequencias++;
 
                 if (i == j) {
-                    soma = (*tab).M[0][0] + (*tab).M[1][1] + (*tab).M[2][2];
+                    soma = tab->M[0][0] + tab->M[1][1] + tab->M[2][2];
                     if (soma == tipo) contSequencias++;
                 }
 
                 if (i +  j == 2) {
-                    soma = (*tab).M[0][2] + (*tab).M[1][1] + (*tab).M[2][0];
+                    soma = tab->M[0][2] + tab->M[1][1] + tab->M[2][0];
                     if (soma == tipo) contSequencias++;
                 }
 
@@ -97,19 +97,19 @@ int regra3(Tabuleiro *tab, int tipo) {
 }
 
 int regra4(Tabuleiro *tab, int tipo, int tipoHumano) {
-    if ((*tab).M[0][0] == tipoHumano && posicaoDisponivel(tab, 2, 2)) {
+    if (tab->M[0][0] == tipoHumano && posicaoDisponivel(tab, 2, 2)) {
         marcaJogada(tab, 2, 2, tipo);
         return 1;
     }
-    if ((*tab).M[0][2] == tipoHumano && posicaoDisponivel(tab, 2, 0)) {
+    if (tab->M[0][2] == tipoHumano && posicaoDisponivel(tab, 2, 0)) {
         marcaJogada(tab, 2, 0, tipo);
         return 1;
     }
-    if ((*tab).M[2][0] == tipoHumano && posicaoDisponivel(tab, 0, 2)) {
+    if (tab->M[2][0] == tipoHumano && posicaoDisponivel(tab, 0, 2)) {
         marcaJogada(tab, 0, 2, tipo);
         return 1;
     }
-    if ((*tab).M[2][2] == tipoHumano && posicaoDisponivel(tab, 0, 0)) {
+    if (tab->M[2][2] == tipoHumano && posicaoDisponivel(tab, 0, 0)) {
         marcaJogada(tab, 0, 0, tipo);
         return 1;
     }
